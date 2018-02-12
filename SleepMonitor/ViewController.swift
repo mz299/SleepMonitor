35f20d77bdafa60eddc25cc87103e835c32051e5
Min Zeng <mz299@njit.edu>
2018-02-11 22:20:55 -0500

    Create project.

---
+++ SleepMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepMonitor
+//
+//  Created by Min Zeng on 11/02/2018.
+//  Copyright © 2018 Min Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
